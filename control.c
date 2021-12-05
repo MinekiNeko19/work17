60e1bcbc4d8d0bf5abbff1290a2f1c4047805114
Sarah Leong <sleong20@stuy.edu>
2021-12-05 13:11:52 -0500

    contains main

--- control.c
+++ control.c
@@ -1,5 +1,24 @@
 #include "control.h"
 
-char ** parse_args(char * line);
-int create(char * line, char * filename);
-int remove(char * line, char * filename);
+int semd;
+
+int parse_args(char * line) {
+    return 0;
+}
+
+int create() {
+    semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+    return errno;
+}
+
+int rem() {
+    semctl(semd,IPC_RMID,0);
+    return errno;
+}
+
+void print_err() {
+    if (errno) {
+        printf("error %d: %s\n", errno, strerror(errno));
+    }
+    errno = 0;
+}
--- write.h
+++ write.h
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "control.h"
 
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/types.h>
-#include <errno.h>
-
-#define KEY 24602
-
-int write(char * line, char * filename);
+int main();
+int write(char * line);
