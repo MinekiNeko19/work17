a898b9e9dfbe85555ccfceb1cb924ebc0d845a73
Sarah Leong <sleong20@stuy.edu>
2021-12-05 13:32:54 -0500

    parse_args decides create or remove

--- control.c
+++ control.c
@@ -1,19 +1,30 @@
 #include "control.h"
 
-int semd;
+int semid;
 
 int parse_args(char * line) {
-    return 0;
+    if (line[0]=='n') {
+        printf("creating semaphore\n");
+        create();
+        print_err();
+    }
+    if (line[0]=='y') {
+        printf("removing semaphore\n");
+        rem();
+        print_err();
+    }
+    return errno;
 }
 
 int create() {
-    semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-    return errno;
+    semid = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+    return semid;
 }
 
 int rem() {
-    semctl(semd,IPC_RMID,0);
-    return errno;
+    semid = semget(KEY,0,0);
+    semctl(semid,IPC_RMID,0);
+    return semid;
 }
 
 void print_err() {
