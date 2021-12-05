7ce8199c4fa8eb82cec04a5bfdb274e07f2585c8
Sarah Leong <sleong20@stuy.edu>
2021-12-05 15:06:35 -0500

    accesses semid and shmid ids and can write into file

--- write.c
+++ write.c
@@ -1,9 +1,24 @@
 #include "write.h"
 
+union semun {
+  int val;                  //used for SETVAL
+  struct semid_ds *buf;     //used for IPC_STAT and IPC_SET
+  unsigned short  *array;   //used for SETALL
+  struct seminfo  *__buf;
+};
 
 int main() {
     // access resources in semaphone
-    
+    parse_args("n");
+    int semid = semget(KEY,0,0);
+    int shmid = shmget(KEY,0,0);
+    int file = open("transcript", O_RDONLY | O_WRONLY | O_APPEND);
+    write(file,"help\n", 5);
+
+    printf("semid: %d\tshmid: %d\n", semid,shmid);
+    // parse_args("y");
+    // close(file);  
+
     // display last line added to file
 
     // prompt for next line
@@ -11,11 +26,13 @@ int main() {
     // write to file and shared mem
 
     // temporary test code
-    char input[3];
-    fgets(input, 3 , stdin);
-    printf("%c\n",input[0]);
-    parse_args(input);
+    // char input[100];
+    // fgets(input, 3 , stdin);
+    // printf("%c\n",input[0]);
+    // parse_args(input);
+
+    
 }
-int write(char * line) {
+int writing(char * line) {
     return 0;
 }
