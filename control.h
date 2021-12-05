17069ea24f3c6814e420876c3c14138be81b1cc8
Sarah Leong <sleong20@stuy.edu>
2021-12-05 13:53:51 -0500

    added libraries for file opening

--- control.h
+++ control.h
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #define KEY 24602
 
