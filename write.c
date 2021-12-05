7fc13a42064fb44eafe09e10bb4a298b7183a013
Sarah Leong <sleong20@stuy.edu>
2021-12-05 13:13:27 -0500

    test code for making sempahors

--- write.c
+++ write.c
@@ -1,3 +1,31 @@
 #include "write.h"
 
-int write(char * line, char * filename);
+
+int main() {
+    // access resources in semaphone
+    
+    // display last line added to file
+
+    // prompt for next line
+
+    // write to file and shared mem
+
+    // temporary test code
+    char input[3];
+    fgets(input, 3 , stdin);
+    printf("%c\n",input[0]);
+    if (input[0]=='n') {
+        printf("creating semaphore\n");
+        create();
+        print_err();
+    }
+    if (input[0]=='y') {
+        printf("removing semaphore\n");
+        rem();
+        print_err();
+    }
+    return 0;
+}
+int write(char * line) {
+    return 0;
+}
