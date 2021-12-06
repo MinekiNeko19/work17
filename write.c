e01b65678e6c5c3425482bb7d4b8103fed8fd66c
Sarah Leong <sleong20@stuy.edu>
2021-12-05 21:36:04 -0500

    shm works; seg fault with file somewhere

--- write.c
+++ write.c
@@ -1,12 +1,10 @@
 #include "write.h"
 
 int main() {
-  char temp[3];
-  fgets(temp,3,stdin);
   parse_args("c");
 
   // access semaphore
-  int semd = semget(KEY,0,0);
+  int semd = semget(SEMKEY,0,0);
   struct sembuf sb;
   sb.sem_num = 0;
   sb.sem_flg = SEM_UNDO;
@@ -14,11 +12,36 @@ int main() {
 
   semop(semd, &sb, 1); //perform the operation
 
+  // access shared memory
+  int shmd = shmget(SHMKEY,0,0);
+  int * data = shmat(shmd,0,0);
+  printf("shmd data: %d\n",*data);
+
+  // access transcript
+  int file = open("transcript", O_APPEND);
+  // char line[100];
+  // read(file,line,*data);
+  // printf("Last Line: %s\n",line);
+  char input[100];
+  printf("Enter Next Line: ");
+  fgets(input,99,stdin);
+  input[100] = '\0';
+  write(file,input,100);
 
   // closing the semaphores
   sb.sem_op = 1; //set the operation to up
   semop(semd, &sb, 1); //perform the operation
-  parse_args("r");
+
+  // closing shared memory
+  shmdt(data);
+
+  // closing file
+  close(file);
+  
+  printf("Finished with the story? (y/n)\n");
+  char temp[3];
+  fgets(temp,3,stdin);
+  parse_args(temp);
 
   return 0;
 }
