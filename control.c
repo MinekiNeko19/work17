d134b28490e9af57130f9c1c5dfec83f747f7abf
Sarah Leong <sleong20@stuy.edu>
2021-12-06 21:00:22 -0500

    removed some testing code

--- control.c
+++ control.c
@@ -16,29 +16,27 @@ void create() {
     // semaphone
     int semd = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0644);
     if (semd == -1) {
-        // printf("error %d: %s\n", errno, strerror(errno));
+        // print_err();
         semd = semget(SEMKEY, 1, 0);
     }
     union semun us;
     us.val = 1;
     int r = semctl(semd, 0, SETVAL, us);
-    printf("Semaphore value: %d\n", r);
+    // printf("Semaphore value: %d\n", r);
 
     // shared memory
     int shmd = shmget(SHMKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
+    // printf("shmd: %d\n", shmd);
     if (shmd == -1) {
-        // printf("error %d: %s\n", errno, strerror(errno));
+        // print_err();
         shmd = shmget(SHMKEY, 0, 0);
     }
-    // int * data = shmat(shmd,0,0);
-    // *data = 0;
-    // printf("shmd: %d\tdata: %d\n",shmd, *data);
-    // shmdt(data);
 
     // file
-    int file = open("transcript", O_CREAT | O_EXCL | O_TRUNC, 0644);
+    int file = open("transcript", O_CREAT | O_TRUNC, 0644);
+    // printf("file: %d\n",file);
     if (file == -1) {
-        // printf("error %d: %s\n", errno, strerror(errno));
+        // print_err();
         file = open("transcript", O_RDWR);
     }
     close(file);
@@ -46,29 +44,24 @@ void create() {
 
 void rem() {
     int semd = semget(SEMKEY,0,0);
-    if (semd == -1) {
-        // printf("error %d: %s\n", errno, strerror(errno));
-        semd = semget(SEMKEY, 1, 0);
-    }
+    // print_err();
     semctl(semd,IPC_RMID,0);
 
     int shmd = shmget(SHMKEY,0,0);
-    if (shmd == -1) {
-        // printf("error %d: %s\n", errno, strerror(errno));
-        shmd = shmget(SHMKEY, 1, 0);
-    }
+    // print_err();
     shmctl(shmd,IPC_RMID,0);
 
     int file = open("transcript", O_RDONLY);
     struct stat sb;
     stat("transcript", &sb);
     char txt[sb.st_size];
-    read(file,txt,sb.st_size-1);
+    read(file,txt,sb.st_size);
     printf("%s\n",txt);
 }
 
 void print_err() {
-   
+   printf("error %d: %s\n", errno, strerror(errno));
+   errno = 0;
 }
 
 int main(int argc, char *argv[]) {
